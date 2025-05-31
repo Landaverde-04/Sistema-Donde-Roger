037858ecf388e9505175eeb9c3daef42f6d82671
RC22009ues <hec159357@gmail.com>
2025-05-31 15:32:56 -0600

    Agregar base de datos Donde_Roger_DB

--- Sistema_Donde_Roger/settings.py
+++ Sistema_Donde_Roger/settings.py
@@ -73,9 +73,13 @@ WSGI_APPLICATION = 'Sistema_Donde_Roger.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "Donde_Roger_DB",
+        "USER": "postgres",
+        "PASSWORD": "1234",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
