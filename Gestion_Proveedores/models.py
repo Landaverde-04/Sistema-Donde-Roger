a4b82114eca3dd862e583775119b9e73fd2c7907
Landaverde-04 <148600843+Landaverde04@users.noreply.github.com>
2025-06-07 15:07:06 -0600

    Barra de navegacion y modelo Proveedor
    Implementación de la navbar basica, para tener un modelo base, luego será completado con los diferentes modulos que vayan surgiendo

--- Gestion_Proveedores/models.py
+++ Gestion_Proveedores/models.py
@@ -2,18 +2,16 @@ from django.db import models
 
 class Proveedor(models.Model):
 
-    idProveedor = models.CharField(max_length=4, primary_key=True) #iria?
+    idProveedor = models.AutoField(primary_key=True)
     nombreEncargado = models.CharField(max_length=30)
     apellidoEncargado = models.CharField(max_length=30)
     nombreEmpresa = models.CharField(max_length=50)
     tieneIva = models.BooleanField(default=False)
     telProveedor = models.CharField(max_length=8)
     duiEncargado = models.CharField(max_length=10)
-    emailProveedor = models.EmailField(max_length=50)#queda como emailfield debido a la validacion de xys@abc.com
+    emailProveedor = models.EmailField(max_length=50)
     ubicacionProveedor = models.TextField()
     logoURL = models.TextField()
-    horarioApertura = models.TimeField() #timefield o charfield o textfield?
-    horarioCierre = models.TimeField()
-
+    
     def __str__(self):
         return self.nombreEncargado+ " - "+ self.nombreEmpresa
