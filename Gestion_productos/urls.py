29920cccef40f4f11a21cb30e432a3653282db47
RC22009ues <hec159357@gmail.com>
2025-06-09 00:48:48 -0600

    insertar en la base de datos producto

--- Gestion_productos/urls.py
+++ Gestion_productos/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('registrar_producto/', views.registrar_producto, name='registrar_producto'),
-    #path('listar_productos/', views.listar_productos, name='listar_productos'),
-    #path('actualizar_producto/<int:producto_id>/', views.actualizar_producto, name='actualizar_producto'),
-    #path('eliminar_producto/<int:producto_id>/', views.eliminar_producto, name='eliminar_producto'),
+    path('listar_productos/', views.listar_productos, name='listar_productos'),
+    path('actualizar_producto/<int:producto_id>/', views.actualizar_producto, name='actualizar_producto'),
+    path('eliminar_producto/<int:producto_id>/', views.eliminar_producto, name='eliminar_producto'),
+    path("detalle_producto/<int:producto_id>/", views.detalle_producto, name="detalle_producto"),
 ]
