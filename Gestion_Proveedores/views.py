072d594e2aafb2e67e9cd71d1de526458e935e08
Landaverde-04 <185978091+Landaverde-04@users.noreply.github.com>
2025-06-13 10:45:33 -0600

    Deshabilitar proveedor listo, validaciones de confirmación
    La función de deshabilitar proveedor ya funciona, también se incluye las validaciones de confirmación a la hora de eliminar proveedor y en el registro de proveedor a la hora de cancelar

--- Gestion_Proveedores/views.py
+++ Gestion_Proveedores/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from Gestion_Proveedores.models import Proveedor, HorarioProveedor
 from django.core.paginator import Paginator
@@ -50,7 +50,7 @@ def registrar_proveedor(request):
 @login_required
 def listar_proveedor(request):
 
-    proveedores = Proveedor.objects.all().order_by('idProveedor')#objeto de proveedor ordenado por id
+    proveedores = Proveedor.objects.filter(estaHabilitadoProveedor=True).order_by('idProveedor')#objeto de proveedor ordenado por id
     paginator = Paginator(proveedores, 10) #usamos la plantilla de django para paginar los proveedores con un maximo de 10 elementos, le pasamos el elemento o listado de proveedores
 
     page_number = request.GET.get('page') #guardamos el # de pagina actual en la url
@@ -58,3 +58,10 @@ def listar_proveedor(request):
 
     return render(request, 'listar_proveedores.html', {'proveedores_paginados': page})#aca en vez de mandar un objeto de proveedores mandamos el de la paginacion que contiene tambien el listado de proveedores
 
+#Este metodo funciona para que se deshabilite el proveedor y ya no aparezca en el cuadro de listado de proveedores, se define su respectiva url y en el html se pasa el argumento de la vista en el boton elimiar, incluyendo un mensaje de confirmación con "oneclick"
+@login_required
+def deshabilitar_proveedor(request, id):#se pasa de argumento el id para que exactamente en la bd busque el id de la fila de la tabla
+    proveedor = get_object_or_404(Proveedor, pk=id) #mandamos el get object 404 para que si no hay id muestre que no existe la pagina
+    proveedor.estaHabilitadoProveedor = False #deshabilitamos el campo de habilitacion del proveedor
+    proveedor.save()
+    return redirect('listar_proveedores') #se redirige a la misma vista de listar proveedores
