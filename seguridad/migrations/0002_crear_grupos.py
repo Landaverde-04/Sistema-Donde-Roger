10186d8dfb34f4de22a27496ebcc8900c9119e1b
Jorge Llanes <lq21001@ues.edu.sv>
2025-06-13 20:26:12 -0700

    Creacion de roles

---
+++ seguridad/migrations/0002_crear_grupos.py
@@ -0,0 +1,25 @@
+from django.db import migrations
+
+def crear_grupos(apps, schema_editor):
+    from django.contrib.auth.models import Group
+    
+    # Crear los grupos si no existen
+    Group.objects.get_or_create(name='Jefe')
+    Group.objects.get_or_create(name='Gerente')
+    Group.objects.get_or_create(name='Colaborador')
+
+def reverse_crear_grupos(apps, schema_editor):
+    from django.contrib.auth.models import Group
+    
+    # Eliminar los grupos (en caso de reversión de la migración)
+    Group.objects.filter(name__in=['Jefe', 'Gerente', 'Colaborador']).delete()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('seguridad', '0001_initial'),  # Asegúrate de que esta dependencia sea correcta
+    ]
+
+    operations = [
+        migrations.RunPython(crear_grupos, reverse_code=reverse_crear_grupos),
+    ]
---
+++ seguridad/models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from empleado.models import Empleado  # Importar el modelo Empleado
+
+class Usuario(AbstractUser):
+    idEmpleado = models.OneToOneField(Empleado, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.username
