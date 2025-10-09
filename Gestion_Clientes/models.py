f305cbead9a4d0c83105cefdf825a8b44ea4b6a6
Kevin Landaverde <landaverde.kevin04@gmail.com>
2025-10-09 13:57:49 -0600

    Implementación de modelo Cliente y Direccion

--- Gestion_Clientes/models.py
+++ Gestion_Clientes/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    idCliente = models.AutoField(primary_key=True)
+    nombreCliente = models.CharField(max_length=30)
+    apellidoCliente = models.CharField(max_length=30)
+    duiCliente = models.CharField(max_length=10)
+    telefonoCliente = models.CharField(max_length=9)
+    emailCliente = models.EmailField(max_length=50)
+    nacimientoCliente = models.DateField()
+    estaHabilitadoCliente = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'Cliente'
+
+    def __str__(self):
+        return self.nombreCliente
+
+class DireccionCliente(models.Model):
+    idCliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    idDireccion = models.AutoField(primary_key=True)
+    direccion = models.TextField()
+
+    class Meta:
+        db_table = 'DireccionCliente'
--- Gestion_Clientes/urls.py
+++ Gestion_Clientes/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('registrar_cliente/', views.registrar_cliente, name='registrar_cliente'),
+    path('listar_clientes/', views.listar_clientes, name='listar_clientes'),
 ]
