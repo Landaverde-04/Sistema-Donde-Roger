5b7915a87fc53301f0c809511a7efa72fa24c296
Landaverde-04 <landaverde.kevin04@gmail.com>
2025-10-13 08:38:45 -0600

    Finalización de lógica registrar direcciones

--- static/JS/registrar_cliente.js
+++ static/JS/registrar_cliente.js
@@ -1,16 +1,41 @@
 function agregarDireccion() {
     // Crear un nuevo elemento textarea
     const contenedor = document.getElementById('direcciones');
-    const cantidad = contenedor.querySelectorAll('textarea').length + 1;
-    const div_direcciones = document.getElementById('div-direccion' + (cantidad - 1));
+    const ultimo = contenedor.querySelector('.direccion:last-of-type') || contenedor.lastElementChild;
+    const cantidad = contenedor.querySelectorAll('.direccion').length + 1;
 
-        div_direcciones.insertAdjacentHTML('afterend', `
+         const html = `
             <div class="campo full" id="div-direccion${cantidad}">
             <label>Dirección adicional:</label>            
             <textarea id='direccion${cantidad}' name="direccion_cliente" rows="1" cols="80"></textarea>
             <button type="button" class="btn btn-danger" onclick="this.parentElement.remove()">Eliminar</button>
             </div>
-        `);
-        contenedor.appendChild(div);
+        `;
+        
+        if(ultimo) {
+            ultimo.insertAdjacentHTML('afterend', html);
+        } else {
+            contenedor.insertAdjacentHTML('beforeend', html);
+        }
+
+        const nuevoTextarea = document.getElementById(`direccion${cantidad}`);
+        if (nuevoTextarea) {
+            nuevoTextarea.focus();
+        }
+
+        document.addEventListener('DOMContentLoaded', () => {
+            const contenedor = document.getElementById('direcciones');
+            if (!contenedor) return;
+
+            contenedor.addEventListener('click', (event) => {
+                if (event.target.classlist.contains('btn-danger')) {
+                    const bloque = event.target.closest('.direccion');
+                    if (bloque) {
+                        bloque.remove();
+                        renumerar(contenedor)
+                    }
+                }
+            });
+        });
     
 }
