277ed6f04a8ecc6c9d548b5bc8762764cdbbc02c
Landaverde-04 <landaverde.kevin04@gmail.com>
2025-10-13 20:35:55 -0600

    HU-49: Inicializacion e implementación de logica eliminar cliente

--- Gestion_Clientes/urls.py
+++ Gestion_Clientes/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('registrar_cliente/', views.registrar_cliente, name='registrar_cliente'),
     path('listar_clientes/', views.listar_clientes, name='listar_clientes'),
+    path('deshabilitar/<int:id>/', views.deshabilitar_cliente, name='deshabilitar_cliente'),
 ]
