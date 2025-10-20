838eb0e7f4477282776aa1960098c79c1cbc8c99
Valentin <ph22012@ues.edu.sv>
2025-10-19 22:00:35 -0600

    Added: Se agrega editar producto, falta modal
    Tiene validaciones y proteccion de rutas, tambien ya guarda los cambios

--- Gestion_Menu/urls.py
+++ Gestion_Menu/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
     path('registrar_producto_menu', views.registrar_producto_menu, name='registrar_producto_menu'),
+    path('editar_producto_menu', views.editar_producto_menu , name='editar_producto_menu_id'),
+    path('editar_producto_menu/<int:productoMenuId>', views.editar_producto_menu, name='editar_producto_menu'),
+    path('editar_producto_menu/<path:invalid>',views.editar_producto_menu , name='editar_producto_menu_default'),
 ]
