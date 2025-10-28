d11ba773aada1a1802fec8a66a74a833a9337520
Valentin <ph22012@ues.edu.sv>
2025-10-28 13:07:56 -0600

    Ya busca solo con el input

--- static/JS/listar_menu.js
+++ static/JS/listar_menu.js
@@ -1,43 +1,48 @@
 
-$(document).ready(function () {
-    const inputBuscar = document.getElementById("inputBuscar"); //Obtener el input de busqueda
-    const btnBuscar = document.getElementById("btnBuscar"); //Obtener el boton de busqueda
-    btnBuscar.addEventListener("click", buscarProducto);
-    function buscarProducto() {
-        const busqueda = inputBuscar.value.toLowerCase();
-        obtenerProductos(busqueda);
-    }
-});
-
-function obtenerProductos(nombre = '', categoria = '') {
-    $.ajax({
-        url: 'api/productos/',
-        type: 'GET',
-        data: {
-            nombre: nombre,
-            categoria: categoria
-        },
-        success: function (response) {
-            var tBody = document.querySelector("#tablaProductos tbody");
-            $(tBody).empty();
-            response.forEach(producto => {
-                const row = tBody.insertRow();
-                row.id = "fila-" + producto.idProductoMenu;
-                const nombre = row.insertCell(0);
-                const precio = row.insertCell(1);
-                const unidad = row.insertCell(2);
-                const descripcion = row.insertCell(3);
-                const acciones = row.insertCell(4);
-
-                nombre.innerText = producto.nombreProductoMenu;
-                precio.innerText = producto.precioProductoMenu;
-                unidad.innerText = producto.tamanioProductoMenu;
-                descripcion.innerText = producto.descripcionProductoMenu;
-                //acciones.innerHTML =;
+    $(document).ready(function () {
+        const inputBuscar = document.getElementById("inputBuscar"); //Obtener el input de busqueda
+        inputBuscar.addEventListener("input", buscarProducto);
+        function buscarProducto() {
+            var busqueda = inputBuscar.value.toLowerCase();
+            if (busqueda.length > 0) {
+                obtenerProductos(busqueda);
+            } else {
+                obtenerProductos();
             }
-            )
-            
         }
-    })
+    });
 
-}
+    function obtenerProductos(nombre = '', categoria = '') {
+        $.ajax({
+            url: 'api/productos/',
+            type: 'GET',
+            data: {
+                nombre: nombre,
+                categoria: categoria
+            },
+            success: function (response) {
+                var tBody = document.querySelector("#tablaProductos tbody");
+                $(tBody).empty();
+                response.forEach(producto => {
+                    const row = tBody.insertRow();
+                    row.id = "fila-" + producto.idProductoMenu;
+                    const nombre = row.insertCell(0);
+                    const precio = row.insertCell(1);
+                    const unidad = row.insertCell(2);
+                    const descripcion = row.insertCell(3);
+                    const acciones = row.insertCell(4);
+
+                    nombre.innerText = producto.nombreProductoMenu;
+                    precio.innerText = producto.precioProductoMenu;
+                    unidad.innerText = producto.tamanioProductoMenu;
+                    descripcion.innerText = producto.descripcionProductoMenu;
+                    acciones.innerHTML = `<a href="/menu/ver/${producto.idProductoMenu}" class="btn btn-info">Ver</a>
+                    <a href="/menu/editar/${producto.idProductoMenu}" class="btn btn-primary">Editar</a>`;
+                 
+                }
+                )
+
+            }
+        })
+
+    }
